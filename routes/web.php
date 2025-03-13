07f905b78191d45bc17f4b2599b726f2b7d7c1d2
Masahiro Kurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2025-03-13 09:36:52 +0900

    define welcome controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('welcome', 'WelcomeController@index');
+Route::get('welcome', [WelcomeController::class, 'index']);

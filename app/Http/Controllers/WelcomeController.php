360651fb7fe8bd40a9c02689b531c1e5204053d1
Masahiro Kurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2025-03-13 09:38:11 +0900

    define index method

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Welcome;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $welcomes = Welcome::all();
+        return view('welcome', ['welcomes' => $welcomes]);
+    }
 }
